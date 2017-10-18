01bfaa523e98137d9c7ee35c88aceadc5ccc8428
wubdut <981128587@qq.com>
2017-10-19 01:13:50 +0800

    lsdjfls

--- XBW/app/shortSwingModule.js
+++ XBW/app/shortSwingModule.js
@@ -5,15 +5,9 @@ define([], function() {
 	
 	coreModule.controller('shortSwingCntrl', function($scope, $http) {
 		$scope.shortSwingList = [];
-		for (var i = 0; i < 5; i++) {
-            $scope.shortSwingList.push({
-				id : '000001',
-				name : '平安银行'
-			});
-		}
         $http.get("http://localhost:8888/").then(function (response) {
         	// var str = JSON.stringify(response);
-            alert(response.data[0].content);
+            $scope.shortSwingList = response.data;
         });
 	});
 });
